087749bed2b9a50e29320329495a4086e48d661d
ashish3121990 <ashish3121990@gmail.com>
2021-09-16 21:58:08 +0530

    Initial commit
    Initial commit

---
+++ demo-aws-ssm-parameter/src/main/java/com/ams/demo_aws_ssm_parameter/App.java
@@ -0,0 +1,13 @@
+package com.ams.demo_aws_ssm_parameter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
