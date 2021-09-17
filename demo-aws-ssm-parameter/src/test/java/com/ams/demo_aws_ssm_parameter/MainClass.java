e914c31c4ca2f33efa3f162d5d759fee55a78172
ashish3121990 <ashish3121990@gmail.com>
2021-09-17 22:25:06 +0530

    update

--- demo-aws-ssm-parameter/src/test/java/com/ams/demo_aws_ssm_parameter/MainClass.java
+++ demo-aws-ssm-parameter/src/test/java/com/ams/demo_aws_ssm_parameter/MainClass.java
@@ -1,19 +1,21 @@
 package com.ams.demo_aws_ssm_parameter;
 
-import org.apache.log4j.BasicConfigurator;
+//import org.apache.log4j.BasicConfigurator;
 
 public class MainClass 
 {
-	public static String dburl = null;
+	public static String dburl,dbpassword = null;
 	
 	public static void main(String args[]) 
 	{
-		BasicConfigurator.configure();
+		//BasicConfigurator.configure();
 		
 		dburl = FetchSSMParameter.Fetch_SSM_Parameter("/my-app/dev/db-url");
+		dbpassword = FetchSSMParameter.Fetch_SSM_Parameter("/my-app/dev/db-password");
 		
+				
 		System.out.println("dburl :- " + dburl);
-		
+		System.out.println("dbpassword :- " + dbpassword);
 		
 		
 		
